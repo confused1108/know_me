2fdf37a43f717a0878b16c4cc070cb124ba15091
confused1108 <ahuja.hits812@yahoo.com>
2018-07-08 01:28:50 +0530

    added editor

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
 	<title>
 		KNow-Me
 	</title>
+	<meta charset="utf-8">
+	<script src="https://cdn.ckeditor.com/4.10.0/standard/ckeditor.js"></script>
 </head>
 <body>
 	<br>
@@ -14,14 +16,19 @@
 
 	<?php
 	} else{ ?>
+	
 	<h3>KNow-Me : A tool to convert a simple page to a URL and QR code for easy sharing without saving to database</h3>
 	<h4>Currently support upto around 2500 characters</h4>
 	<form action="geturl.php" method="post">
-		<textarea cols="50" rows="10" name="content">Remove this text and type</textarea>
+		<textarea name="content"></textarea>
+		<script>
+			CKEDITOR.replace( 'content' );
+		</script>
 		<input type="submit">
 	</form>
 	<?php
 	}
 	?>
+		
 </body>
 </html>
