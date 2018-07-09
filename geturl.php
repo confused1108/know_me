f2124e846dd85189c01284dd288faf02adcf8b21
confused1108 <ahuja.hits812@yahoo.com>
2018-07-09 23:16:19 +0530

    modified url

--- geturl.php
+++ geturl.php
@@ -1,9 +1,10 @@
 <?php
 $content=$_POST['content'];
 $encode=base64_encode($content);
+$encode=substr($encode,4);
 ?>
 <br>
 <h4>URL:</h4>
-<input value="mysite.com?view=<?php echo $encode; ?>" style="width:80%;">
+<input value="techon.esy.es/knowme/index.php?view=<?php echo $encode; ?>" style="width:80%;">
 <h4>QR code</h4>
-<iframe height="300px" width="300px" src="https://api.qrserver.com/v1/create-qr-code/?size=300x300&data=mysite.com?view=<?php echo $encode; ?>"></iframe>
+<iframe height="300px" width="300px" src="https://api.qrserver.com/v1/create-qr-code/?size=300x300&data=techon.esy.es/knowme/index.php?view=<?php echo $encode; ?>"></iframe>
